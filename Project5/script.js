080f12f7095c83b4298f6df7bf5990b6d949cee1
Himanshu <technophilic000@gmail.com>
2022-03-05 10:34:59 +0530

    Project5

--- Project5/script.js
+++ Project5/script.js
@@ -13,11 +13,13 @@ navToggle.addEventListener('click',function(){
     // console.log(links.classList.contains('random'));
     // console.log(links.classList.contains('links'));
 
-if(links.classList.contains('show-links')){
-    links.classList.remove('show-links');
-}
-else{
-    links.classList.add('show-links');
-}
+// if(links.classList.contains('show-links')){
+//     links.classList.remove('show-links');
+// }
+// else{
+//     links.classList.add('show-links');
+// }
+
+links.classList.toggle('show-links');
 
 })
